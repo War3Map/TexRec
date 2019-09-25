081d86f9492c72668eb5f12f444a914153b8bc32
War3Map <ivan.morzhov@yandex.ru>
2019-09-25 15:17:13 +1000

    Шаблоны классов для работы с текстовыми документами и нейросетью.

---
+++ WordProcessor/Wordprocessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace WordProcessor
+{
+    public class WordProcessor
+    {
+
+        static void WriteInDocFile(string content,string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        static string ReadTextFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
